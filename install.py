5ac31df6f277a835f60699b323aba1fff0fad10b
Mathew Lewis <mathewlewis@Mathews-MacBook-Pro.local>
2020-05-19 02:35:39 -0700

    .

--- install.py
+++ install.py
@@ -12,60 +12,68 @@ def Print(s,clear=True):
 url = 'https://raw.githubusercontent.com/mathewdblewis/passwordManager/master/passwords.py'
 
 
-# get dependencies
-Print("INSTALLING DEPENDENCIES",clear=False)
-try: system("pip3 install cryptography")
-except:
-	print("could not install dependency 'cryptography'")
-	exit(1)
-try: system("pip3 install pyperclip")
-except: pass
-
-
-# get file
-file = ""
-try: file = urllib.request.urlopen(url).read().decode()
-except:
-	print("the source code could not be found")
-	exit(1)
-file = '#!'+call('which python3',shell=True).decode()+'\n'+'\n'.join(file.split('\n')[1:])
-
-
-# get path
-Print("INSTALLING PASSWORD MANAGER",clear=False)
-default,paths = "",call('echo $PATH',shell=True).decode().split(':')
-for path in paths:
-	try: open(path+'/passwords','w')
-	except: continue
-	default = path+'/passwords'
-	break
-
-path = ""
-while True:
-	print('Give the full path of where you would like to put this program.')
-	print('You must provide an absolute path',end='')
-	if default!='': print('the path "' + default + '" is recommended.')
-	else: print(".")
-	if default!='': print('Press enter now to use the default or ')
-	path = input('type out your prefered path here: ')
-	if path == '': path = default
-	if path[0] != '/':
-		print("ERROR: you must provide an absolute path")
-		continue
-	try: open(path,'w')
+def install():
+	# get dependencies
+	Print("INSTALLING DEPENDENCIES",clear=False)
+	try: system("pip3 install cryptography")
 	except:
-		print("ERROR: this file cannot be written to, give a different path")
-		continue
-	break
+		print("could not install dependency 'cryptography'")
+		exit(1)
+	try: system("pip3 install pyperclip")
+	except: pass
 
 
-# install program
-open(path,'w').write(file)
-call('chmod 700 ' + path, shell=True)
+	# get file
+	file = ""
+	try: file = urllib.request.urlopen(url).read().decode()
+	except:
+		print("the source code could not be found")
+		exit(1)
+	file = '#!'+call('which python3',shell=True).decode()+'\n'+'\n'.join(file.split('\n')[1:])
 
 
-# prompt the user
-Print("ALL DONE!",clear=False)
-print("passwords has been installed!")
-print("To run the CLI, enter '" + path.split('/')[-1] + "' below")
+	# get path
+	Print("INSTALLING PASSWORD MANAGER",clear=False)
+	default,paths = "",call('echo $PATH',shell=True).decode().split(':')
+	for path in paths:
+		try: open(path+'/passwords','w')
+		except: continue
+		default = path+'/passwords'
+		break
+
+	path = ""
+	while True:
+		print('Give the full path of where you would like to put this program.')
+		print('You must provide an absolute path',end='')
+		if default!='': print('the path "' + default + '" is recommended.')
+		else: print(".")
+		if default!='': print('Press enter now to use the default or ')
+		path = input('type out your prefered path here: ')
+		if path == '': path = default
+		if path[0] != '/':
+			print("ERROR: you must provide an absolute path")
+			continue
+		try: open(path,'w')
+		except:
+			print("ERROR: this file cannot be written to, give a different path")
+			continue
+		break
+
+
+	# install program
+	open(path,'w').write(file)
+	call('chmod 700 ' + path, shell=True)
+
+
+	# prompt the user
+	Print("ALL DONE!",clear=False)
+	print("passwords has been installed!")
+	print("To run the CLI, enter '" + path.split('/')[-1] + "' below")
+	exit()
+
+
+install()
+
+
+
 
--- passwords.py
+++ passwords.py
@@ -11,6 +11,8 @@ if version[0]!='3':
 
 # imports
 
+from subprocess import check_output as call
+import urllib.request; from os import system
 import json; from getpass import getpass; import sys
 from os import system, remove, urandom
 from os.path import expanduser,realpath
@@ -26,6 +28,9 @@ except: pass
 
 fileName = expanduser('~')+'/.passwordmanager.enc'
 saltLen = 16
+# url of passwords.py script on github
+url = 'https://raw.githubusercontent.com/mathewdblewis/passwordManager/master/passwords.py'
+
 
 
 # utilities
@@ -347,9 +352,67 @@ def editEntry(params):
 	return ('viewEntry',data,service)
 
 
+def install():
+	# get dependencies
+	Print("INSTALLING DEPENDENCIES",clear=False)
+	try: system("pip3 install cryptography")
+	except:
+		print("could not install dependency 'cryptography'")
+		exit(1)
+	try: system("pip3 install pyperclip")
+	except: pass
+
+	# get file
+	file = ""
+	try: file = urllib.request.urlopen(url).read().decode()
+	except:
+		print("the source code could not be found")
+		exit(1)
+	file = '#!'+call('which python3',shell=True).decode()+'\n'+'\n'.join(file.split('\n')[1:])
+
+	# get path
+	Print("INSTALLING PASSWORD MANAGER",clear=False)
+	default,paths = "",call('echo $PATH',shell=True).decode().split(':')
+	for path in paths:
+		try: open(path+'/passwords','w')
+		except: continue
+		default = path+'/passwords'
+		break
+
+	path = ""
+	while True:
+		print('Give the full path of where you would like to put this program.')
+		print('You must provide an absolute path',end='')
+		if default!='': print('the path "' + default + '" is recommended.')
+		else: print(".")
+		if default!='': print('Press enter now to use the default or ')
+		path = input('type out your prefered path here: ')
+		if path == '': path = default
+		if path[0] != '/':
+			print("ERROR: you must provide an absolute path")
+			continue
+		try: open(path,'w')
+		except:
+			print("ERROR: this file cannot be written to, give a different path")
+			continue
+		break
+
+	# install program
+	open(path,'w').write(file)
+	call('chmod 700 ' + path, shell=True)
+
+	# prompt the user
+	Print("ALL DONE!",clear=False)
+	print("passwords has been installed!")
+	print("To run the CLI, enter '" + path.split('/')[-1] + "' below")
+	exit()
+
+
+
 
 if __name__ == '__main__':
 	devmode = len(sys.argv) == 2 and sys.argv[1] == '-devmode'
+	if len(sys.argv) == 2 and sys.argv[1] == '-install': install()
 	state = ('load',)
 	try:
 		while True:
